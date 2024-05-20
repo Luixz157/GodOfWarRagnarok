fa42a667db009972896ce5126690ea36f019827b
Luiz Antonio Souza <luiz.antoniodesouza004@gmail.com>
2024-05-20 10:37:17 -0300

    GodOfWar

---
+++ js/all.js
@@ -0,0 +1,1 @@
+"use strict";var swiper=new Swiper(".slide-characters",{slidesPerView:3.5,spaceBetween:19,freeMorde:!0,breakpoints:{320:{slidesPerView:1.1,spaceBetween:19},768:{slidesPerView:2.2,spaceBetween:19},991:{slidesPerView:2.8,spaceBetween:19},1200:{slidesPerView:3.5,spaceBetween:19}}});
